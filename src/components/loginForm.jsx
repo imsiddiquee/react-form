b246de20fb669de8d984ba641358379023b7107a
Shamim <imsiddiquee@gmail.com>
2019-04-08 02:32:40 +0600

    Extract input element and apply in login page

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Input from "./common/input";
 
 class LoginForm extends Component {
   state = {
@@ -21,30 +22,20 @@ class LoginForm extends Component {
         <h1>Login</h1>
 
         <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="username">User Name</label>
-            <input
-              value={account.username}
-              onChange={this.handleChange}
-              name="username"
-              type="text"
-              className="form-control"
-              id="username"
-              placeholder="User Name"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              value={account.password}
-              onChange={this.handleChange}
-              name="password"
-              type="password"
-              className="form-control"
-              id="password"
-              placeholder="Password"
-            />
-          </div>
+          <Input
+            name="username"
+            label="User Name"
+            value={account.username}
+            type="text"
+            onChange={this.handleChange}
+          />
+          <Input
+            name="password"
+            label="Password"
+            value={account.password}
+            type="password"
+            onChange={this.handleChange}
+          />
 
           <button className="btn btn-primary">Login</button>
         </form>
