0882d6f8dfbb95eb0f3bf311bf381e62dcda139b
Shamim <imsiddiquee@gmail.com>
2019-04-08 13:56:04 +0600

    Dynamically apply property on controls by used the ...rest

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,18 +1,10 @@
 import React from "react";
 
-const Input = ({ name, label, value, type, error, onChange }) => {
+const Input = ({ name, label, error, ...rest }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
-      <input
-        value={value}
-        onChange={onChange}
-        name={name}
-        type={type}
-        className="form-control"
-        id={name}
-        placeholder="User Name"
-      />
+      <input {...rest} name={name} id={name} className="form-control" />
       {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
