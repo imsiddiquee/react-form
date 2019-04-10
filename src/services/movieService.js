0491f4a83eb1f67bc249626faaccd6691c65c102
Shamim <imsiddiquee@gmail.com>
2019-04-10 10:23:24 +0600

    removed all fake service implement node service.

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -3,10 +3,28 @@ import { apiUrl } from "../config.json";
 
 const apiEndpoint = apiUrl + "movies";
 
+function movieUrl(id) {
+  return `${apiEndpoint}/${id}`;
+}
+
 export function getMovies() {
   return http.get(apiEndpoint);
 }
 
+export function getMovie(movieId) {
+  return http.get(movieUrl(movieId));
+}
+
 export function deleteMovie(movieId) {
-  return http.delete(apiEndpoint + "/" + movieId);
+  return http.delete(movieUrl(movieId));
+}
+
+export function saveMovie(movie) {
+  console.log("movie", movie);
+  if (movie._id) {
+    const body = { ...movie };
+    delete body._id;
+    return http.put(movieUrl(movie._id), body);
+  }
+  http.post(apiEndpoint, movie);
 }
